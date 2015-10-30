d97eb5de35e990bd130bd884e5961094f7ac7231
jonatans <jonatan@jschumacher.com>
2015-10-30 18:12:04 -0400

    WIP - some callback stuff is not working right now

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,5 +1,37 @@
+/*
+*
+*here is the json of my blog (GET request with someone else's API key)
+*https://api.tumblr.com/v2/blog/js-on-js.tumblr.com/posts/text?api_key=fuiKNFp9vQFvjLNvx4sUwti4Yb5yGutBN4Xh10LXZhhRKjWlV4
+*MORE DOCUMENTATION HERE: https://www.tumblr.com/docs/en/api/v2
+*
+*/
+
+//references:
 var fs = require("fs");
 var ejs = require("ejs");
+var tumblr = require('tumblr.js');
+var secrets = require('./secrets.js');
+
+
+//access secrets.js
+var client = tumblr.createClient(secrets.TUMBLR_CONFIG);
+
+//posts[]
+//need to callback()
+//var posts = getTumblrPosts({blogName: "js-on-js", numDays: 7});
+var posts = getTumblrPosts();
+
+
+console.log(posts + " is " + typeof posts);
+console.log(Array.isArray(posts));
+
+console.log("Number of new posts: " + posts.length);
+posts.forEach(function(p){
+	console.log(p);
+	console.log(p.date + "\t" + p.title);
+
+});
+
 
 //Read CSV file with Contacts:
 var csvFile = fs.readFileSync("./data/friend_list.csv","utf8");
@@ -8,28 +40,64 @@ console.log(csvFile);
 //Parse Contacts in contacts[]:
 var contacts = csvParse(csvFile);
 
-// ///OLD: loop through array of contacts to customize HTML email:
-// contacts.forEach(function(c){
-// 		var emailBody = fs.readFileSync("views/email_template.html");
-// 		emailBody = customizeEmail(c, emailBody.toString());
-// 		console.log("Email: \n" + emailBody);
-// });
+
 
 //Loop through contacts[] to customize email ejs
-var emailTemplate = fs.readFileSync("views/email_template.html").toString();
+var emailTemplate = fs.readFileSync("views/email_template.ejs").toString();	//toString is important!
 contacts.forEach(function(c){
 	console.log(c.firstName + ": " + c.numMonthsSinceContact + '\n');
-	
+	var moreData = {};
+	moreData.firstName = c.firstName;
+	moreData.numMonthsSinceContact = c.numMonthsSinceContact;
+	moreData.latestPosts = [];
+	//todo: add recent blog posts
+
 	//var customizedTemplate = ejs.render(emailTemplate, {firstName: "Jonatan", numMonthsSinceContact: "5"});
-	var customizedTemplate = ejs.render(emailTemplate, c);
-	console.log(customizedTemplate);
+	var customizedTemplate = ejs.render(emailTemplate, moreData);
+	//console.log(customizedTemplate);
 });
 
 
 
+function getTumblrPosts() {
+	var blogName = "js-on-js";
+	var maxPostAge = 7;
+	if(arguments.length > 0)
+	{
+		blogName = arguments[0].blogName || blogName;
+		maxPostAge = arguments[0].numDays || maxPostAge;
+	}
+	console.log("maxAge " + maxPostAge);
 
+//need callback here:
 
+	return client.posts(blogName, function(err, blog){
 
+	var arrNewPosts = [];
+	  //console.log(blog);
+	  console.log("num posts: " + blog.posts.length +"\n\n");
+	  for(var post in blog.posts) {
+	  	//calculate post age:
+	  	var today = new Date();
+	  	var postDate = new Date(blog.posts[post].date);
+	  	var postAge = Math.ceil((today - postDate) / (1000 * 3600 * 24));
+	  	console.log("Post Age: " + today + "-" + postDate + " = " + postAge);
+	  	if(Number(postAge) <= Number(maxPostAge)) {
+	  		console.log("LIHLILLKJL:JL:LSLSJ");
+	  		var p = {
+	  			"date": blog.posts[post].date,
+	  			"title" : blog.posts[post].title,
+	  			"url" : blog.posts[post].post_url
+	  		}
+	  		arrNewPosts.push(p);
+	  		console.log("return array sizeONE: " + arrNewPosts.length);
+	  	}
+	  }
+
+		console.log("return array sizeTWO: " + arrNewPosts.length);
+		return arrNewPosts;
+	});
+}
 
 
 function csvParse(csvFile) {
@@ -74,3 +142,12 @@ function toSnakeCase(str){
 	var modStr =  str.replace(/([A-Z])/g, function($1){return "_"+$1.toLowerCase();}).toUpperCase();
 	return modStr;
 };
+
+
+
+// ///OLD: loop through array of contacts to customize HTML email:
+// contacts.forEach(function(c){
+// 		var emailBody = fs.readFileSync("views/email_template.html");
+// 		emailBody = customizeEmail(c, emailBody.toString());
+// 		console.log("Email: \n" + emailBody);
+// });
